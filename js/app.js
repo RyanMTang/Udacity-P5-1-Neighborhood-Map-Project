0f912cefe98d4fd1999bf883c823b09f749b17c8
unknown <rtang91@gmail.com>
2015-09-23 15:05:17 -0400

    make all markers appear on map

--- js/app.js
+++ js/app.js
@@ -1,34 +1,41 @@
-var place = function(name,lat,lng) {
-  this.name = name,
-  this.lat = lat,
-  this.lng = lng
-};
-
-var model = [
-  new place('El Camino', 45.415687, -75.688241)
-];
-
-
 function initMap() {
-  var myLatLng = {lat: 45.4214, lng: -75.6919};
-
+  // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: myLatLng
+    center: {lat: 45.4214, lng: -75.6919},
+    scrollwheel: true,
+    zoom: 15
   });
 
-  for(var i=0; i<model.length; i++) {
-    var marker = new google.maps.Marker({
-      position: {lat: model[i].lat, lng: model[i].lng},
-      map: map,
-      title: 'Hello World!'
-    });
-  }
+   map.data.setStyle(function(feature) {
+    return {
+      title: feature.getProperty('name'),
+      optimized: false
+    };
+  });
+  map.data.addGeoJson(places);
 }
 
-var ViewModel = function() {
-      var self = this;
-
+var places = {
+  type: 'FeatureCollection',
+  features: [{
+    type: 'Feature',
+    geometry: {type: 'Point', coordinates: [-75.6879, 45.4149]},
+    properties: {name: 'El Camino'}
+  }, {
+    type: 'Feature',
+    geometry: {type: 'Point', coordinates: [-75.688577, 45.413891]},
+    properties: {name: 'Nature Museum'}
+  }, {
+    type: 'Feature',
+    geometry: {type: 'Point', coordinates: [-75.691868, 45.427893]},
+    properties: {name: 'Cacao 70'}
+  }, {
+    type: 'Feature',
+    geometry: {type: 'Point', coordinates: [-75.691273, 45.422303]},
+    properties: {name: 'Ottawa City Hall'}
+  }, {
+    type: 'Feature',
+    geometry: {type: 'Point', coordinates: [-75.692169, 45.428032]},
+    properties: {name: 'Vittoria Trattoria'}
+  }]
 };
-
-    ko.applyBindings(new ViewModel());

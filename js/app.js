60e40b1ac48ff8eb97432e72b41a9ba078c38783
unknown <rtang91@gmail.com>
2015-09-24 03:40:18 -0400

    attempt to clear markers

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+var markers = [];
 var places = [
   {
     name: 'El Camino',
@@ -7,34 +8,46 @@ var places = [
     name: 'Nature Museum',
     latlng: {lat: 45.412976, lng: -75.688523}
   }
-]
+];
 
 var viewModel = function(){
   var self = this;
+
+  self.points= ko.observableArray(places);
 
   self.Map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 45.4214, lng: -75.6919},
     zoom: 15
   });
 
-  self.placeContainer = [];
-  places.forEach(function(place) {
-    self.placeContainer.push(place);
-    var marker = new google.maps.Marker({
-      position: place.latlng,
-      map: self.Map,
-      title: place.name
+  for(var i=0; i<self.points().length; i++){
+      var marker = new google.maps.Marker({
+      position: self.points()[i].latlng,
+      title: self.points().name,
+      map: self.Map
     });
-  });
+    markers.push(marker);
+  };
+  
+  function clearMarkers(){
+    for (var i=0; i<markers.length; i++) {
+      if (markers[i].title.toLowerCase().index(self.userInput()) >=0) {
+        markers[i].setMap(null);
+      }
+    }
+  }
+
+  
+
+  
 
 
-  self.points= ko.observableArray(places);
+  self.userInput = ko.observable('');
 
-  self.query = ko.observable('');
 
   self.search = ko.computed(function(){
     return ko.utils.arrayFilter(self.points(), function(point){
-      return point.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
+      return point.name.toLowerCase().indexOf(self.userInput().toLowerCase()) >= 0;
     });
   });
 }

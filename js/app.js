e12c6248856b0853289149d7fe22beac6310f1f4
unknown <rtang91@gmail.com>
2015-09-24 14:51:23 -0400

    add marker filter

--- js/app.js
+++ js/app.js
@@ -1,55 +1,71 @@
-var markers = [];
-var places = [
-  {
-    name: 'El Camino',
-    latlng: {lat: 45.415416, lng: -75.688027}
-  },
-  {
-    name: 'Nature Museum',
-    latlng: {lat: 45.412976, lng: -75.688523}
-  }
-];
-
 var viewModel = function(){
   var self = this;
-
-  self.points= ko.observableArray(places);
+  var infowindow = new google.maps.InfoWindow();
 
   self.Map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 45.4214, lng: -75.6919},
     zoom: 15
   });
 
-  for(var i=0; i<self.points().length; i++){
-      var marker = new google.maps.Marker({
-      position: self.points()[i].latlng,
-      title: self.points().name,
-      map: self.Map
+  var Point = function (map, name, lat, lon, text) {
+    var marker;
+
+    this.name = ko.observable(name);
+    this.lat  = ko.observable(lat);
+    this.lon  = ko.observable(lon);
+    this.text = ko.observable(text);
+
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(lat, lon),
+      animation: google.maps.Animation.DROP
     });
-    markers.push(marker);
-  };
-  
-  function clearMarkers(){
-    for (var i=0; i<markers.length; i++) {
-      if (markers[i].title.toLowerCase().index(self.userInput()) ===-1) {
-        markers[i].setMap(null);
+
+
+    this.isVisible = ko.observable(false);
+
+    this.isVisible.subscribe(function(currentState) {
+      if (currentState) {
+        marker.setMap(map);
+      } else {
+        marker.setMap(null);
       }
-    }
+    });
+
+    this.isVisible(true);
+
+    google.maps.event.addListener(marker, 'click', (function(marker)  {
+            return function() {
+
+                // set info window with a title and open the info window
+                //infowindow.setTitle(marker.title);
+                map.panTo(marker.getPosition());
+                infowindow.setContent(Point.name);
+                infowindow.open(map, marker);          
+
+            };
+        })(marker));
   }
 
+
+  self.query = ko.observable('');
   
+  self.points = ko.observableArray ([
+  new Point(self.Map, 'El Camino', 45.415627, -75.688070, 'Hi'),
+  new Point(self.Map, 'Nature Museum', 45.412961, -75.688534, 'Hi')
+  ]);
 
-  
+  self.filterPoints = ko.computed(function () {
+    var search  = self.query().toLowerCase();
 
+    return ko.utils.arrayFilter(self.points(), function (point) {
+        var doesMatch = point.name().toLowerCase().indexOf(search) >= 0;
 
-  self.userInput = ko.observable('');
+        point.isVisible(doesMatch);
 
-
-  self.search = ko.computed(function(){
-    return ko.utils.arrayFilter(self.points(), function(point){
-      return point.name.toLowerCase().indexOf(self.userInput().toLowerCase()) >= 0;
+        return doesMatch;
     });
   });
+
 }
 
 ko.applyBindings(viewModel);

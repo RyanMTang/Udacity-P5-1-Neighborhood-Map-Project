ca81b8a02fb0e95fdab8d8794674727d0767c514
unknown <rtang91@gmail.com>
2015-10-14 00:40:08 -0400

    fix google maps async and seperate initMap from viewModel

--- js/app.js
+++ js/app.js
@@ -1,16 +1,10 @@
-var infowindow;
-var marker;
 var markerList = ko.observableArray([]);
-var windowContent;
-var filterPoints;
-var points = ko.observableArray([]);
 
 function initMap(){
   //Create map and set initial coordinates and zoom level
   this.Map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 45.4214, lng: -75.6919},
     zoom: 12,
-    scrollWheel: false
   });
   var center = self.Map.getCenter();
   //Map resizes and centers when the window is resized
@@ -19,10 +13,9 @@ function initMap(){
     self.Map.setCenter(center); 
   });
 
-  var infowindow = new google.maps.InfoWindow();
+  this.infowindow = new google.maps.InfoWindow();
   //Function for creating points at different locations on the map
   var Point = function (map, name, lat, lon, id, marker) {
-    var marker;
     var markerLat = lat;
     var markerLon = lon;
     var markerName = name;
@@ -42,51 +35,44 @@ function initMap(){
     //Pushes marker into an observable array
     markerList.push(marker);
 
-    this.isVisible = ko.observable(false);
-
-    //Determines whether or not marker appears on the map
-    this.isVisible.subscribe(function(currentState) {
-      if (currentState) {
-        marker.setMap(map);
-      } else {
-        marker.setMap(null);
-      }
-    });
-
-    this.isVisible(true);
-
     //Fetches information from foursquare on marker click
     google.maps.event.addListener(marker, 'click', (function(marker)  {
             return function() {
 
-                var url = 'https://api.foursquare.com/v2/venues/search'+
+              $.ajax({
+                url: 'https://api.foursquare.com/v2/venues/search'+
                   '?client_id=PRENND0ZKZI4DRHMGHP5GMBASHGUI5FYL2R53VK5J5VVGQCB'+
                   '&client_secret=VHTNW4NDFQSZACFPINV0OC2YEWAISSPXTOHY1UIOZIFVGDLO'+
                   '&v=20130815'+
                   '&ll=' + markerLat + ',' + markerLon +
-                  '&query=' + markerName;
-
-                  $.getJSON(url, function(response){
+                  '&query=' + markerName,
+                dataType: 'json',
+                success: function(response) {
                       var venue = response.response.venues[0];
                       var venueName = venue.name;
                       var venuePhone = venue.contact.formattedPhone;
                       var venueAddress = venue.location.formattedAddress;
-                      var windowContent ='<p>' + venueName + '</p>' + '<p>' + venuePhone + '</p>' + '<p>' + venueAddress + '</p>'
+                      var venueId = venue.id;
+                      var windowContent ='<div id="window">' + '<a href="https://foursquare.com/v/' + venueId + '">' + venueName + '</a>' + '<p>' + venuePhone + '</p>' + '<p>' + venueAddress + '</p>'+'</div>';
                       infowindow.setContent(windowContent);
-          
-                  });//End of .getJSON
+                },
+                error: function(){
+                  alert('Unable to retrieve Fourquare data');
+                }
 
+              });
+                          
 
                 //Open infowindow and pan to clicked marker
                 marker.setAnimation(google.maps.Animation.BOUNCE); //Makes marker bounce
-                setTimeout(function(){marker.setAnimation(null); }, 750) //Makes marker stop bouncing after one bounce
+                setTimeout(function(){marker.setAnimation(null); }, 750); //Makes marker stop bouncing after one bounce
                 map.panTo(marker.getPosition());
                 infowindow.open(map, marker);        
 
             }; //End of return function()
         })(marker)); //End of click event listener
 
-  } //End of Point function
+  }; //End of Point function
 
    //Model with all points
   points = ko.observableArray ([
@@ -97,7 +83,7 @@ function initMap(){
   new Point(this.Map, 'Ottawa City Hall', 45.443206, -75.659874),
   new Point(this.Map, 'Canadian War Museum', 45.417355, -75.716931)
   ]);
-};
+}
 
 //Start viewModel
 var viewModel = function(){
@@ -107,27 +93,65 @@ var viewModel = function(){
   self.listClick = function(clicked){
       var pos = markerList().indexOf(this);
       google.maps.event.trigger(markerList()[pos], 'click');
-    }
+    };
 
- self.query = ko.observable('');
+  self.query = ko.observable('');
 
   //Filters list items and markers based on user input in the search bar
   self.filterPoints = ko.computed(function () {
     var search  = self.query().toLowerCase();
     if (!search) {
+      for (i=0; i<markerList().length; i++) {
+        markerList()[i].setMap(this.Map);
+        infowindow.close(this.Map);
+      }
       return markerList();
     } else {
-    return ko.utils.arrayFilter(points(), function (point) {
-        var doesMatch = point.name().toLowerCase().indexOf(search) >= 0;
-
-        point.isVisible(doesMatch);
+    return ko.utils.arrayFilter(markerList(), function (marker) {
+        var doesMatch = marker.name.toLowerCase().indexOf(search) >= 0;
+        if (doesMatch){
+          marker.setMap(this.Map);
+        } else {
+          marker.setMap(null);
+        }
         return doesMatch;
       });
     }
   });
   //End self.filterPoints
 
+  //The values from the drop down menu
+  self.selected = ko.observable('');
 
+  //Clears all markers off the map
+  function clearOverlays() {
+      for (var i = 0; i < markerList().length; i++ ) {
+      markerList()[i].setMap(null);
+    }
+  }
 
-}
+  //Pulls up the info window for the point selected in the drop down menu
+  //Puts the markers back on the map if the drop down menu value is null
+  self.onChange = function() {
+      var position = self.points().indexOf(self.selected()); 
+      if (self.selected()==null) {
+
+        for (var i = 0; i < markerList().length; i++ ) {
+        markerList()[i].setMap(self.Map);
+        infowindow.close(self.Map, markerList()[i]);
+        self.Map.setCenter(center); 
+        }
+        return;
+      }
+      clearOverlays();
+      markerList()[position].setMap(self.Map);
+      google.maps.event.trigger(markerList()[position], 'click'); 
+  };
+
+  //Reset button that sets the value for the drop down menu to null
+  self.resetSelection = function () {
+    self.selected(null);
+  }
+
+};
 ko.applyBindings(viewModel);
